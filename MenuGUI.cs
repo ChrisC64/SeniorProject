db4d35b9a304fb12929c8226b8270515b087fccf
Valtiaalle <ldiana.zhoukko@gmail.com>
2013-06-19 17:38:41 -0600

    Update MenuGUI.cs
    Changed some box values and added some more comments

--- MenuGUI.cs
+++ MenuGUI.cs
@@ -6,35 +6,40 @@ public class MenuGUI : MonoBehaviour
 	
 	void OnGUI()	
 		{
-			GUI.BeginGroup(new Rect(Screen.width/2 - 50, Screen.height / 2 - 50, 150, 160));
+			
+			// begins the group for the menu
+			GUI.BeginGroup(new Rect(Screen.width/2 - 100, Screen.height/2 - 150, 200, 280));
+				
 				// creates the menu box	
-				GUI.Box(new Rect(0,0,150,160), "Menu");
+				GUI.Box(new Rect(0,0,200,280), "Menu");
 	
 				// creates the buttons. If pressed, will load scene accordingly
 	
 				// start game button		
-				if(GUI.Button(new Rect(30,40,80,20), "Start Game"))		
+				if(GUI.Button(new Rect(50,50,100,30), "Start Game"))		
 				{		
 					Application.LoadLevel("Game");		
 				}
 			
 				// options button		
-				if(GUI.Button(new Rect(30,70,80,20), "Options"))
+				if(GUI.Button(new Rect(50,100,100,30), "Options"))
 				{		
 					Application.LoadLevel("Options");		
 				}		
 			
 				// credits button
-				if(GUI.Button (new Rect(30,100,80,20), "Credits"))
+				if(GUI.Button (new Rect(50,150,100,30), "Credits"))
 				{
 					Application.LoadLevel("Credits");
 				}
 			
 				// quit game button
-				if(GUI.Button (new Rect(30,130,80,20), "Quit Game"))
+				if(GUI.Button (new Rect(50,200,100,30), "Quit Game"))
 				{
 					Application.LoadLevel("Quit Game");
 					//Application.Quit();
+					// switch to quit game later
+					// Quit Game level for testing purpose
 				}
 			
 			GUI.EndGroup ();
