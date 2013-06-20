c532e60d08b4d2edfd25593688e32281bf46b6cb
Valtiaalle <ldiana.zhoukko@gmail.com>
2013-06-20 12:54:48 -0600

    Update flashlightScript.cs
    Got rid of lines we did not need

--- flashlightScript.cs
+++ flashlightScript.cs
@@ -1,47 +1,37 @@
-/*Script for the flashlight PlayerItem
-*Still needs fix for item to toggle visibility
-I somehow messed that up again...
-*/
+/* Script for the flashlight PlayerItem
+ * Make sure to drag the game object from the hierachy 
+ * and not the project panel or select from the 
+ * drop-down tab
+ * The same applies for the other items
+ * Don't make the same mistake I did!
+ */
 
 using UnityEngine;
 using System.Collections;
 
 public class flashlightScript : MonoBehaviour 
 {
-  public GameObject flashlight;	
+	public GameObject flashlight;
+
 	// Use this for initialization
 	void Start () 
 	{
-//		renderer.enabled = false;			
-		//flashlight.SetActive(false);		
-		flashlight.renderer.enabled = false;		
-	}	
+		flashlight.renderer.enabled = false;
+	} // End Start
+	
 	// Update is called once per frame
 	void Update () 
-	{		
+	{
 		if(Input.GetKeyDown("1") && flashlight.renderer.enabled == false)
 		{
-//			renderer.enabled = true;
-//			Debug.Log ("Equipped" + PlayerItems[0].name);
-			
+			Debug.Log ("I haz flashlight");	
 			flashlight.renderer.enabled = true;
-			//flashlight.SetActive(true);
-			
-//			inventoryScript equippedItem = gameObject.GetComponent<inventoryScript>();
-//			equippedItem.
-			
-//			equippedItem = flashlight;			
 		}
 		else if(Input.GetKeyDown("1") && flashlight.renderer.enabled == true)
 		{
-			Debug.Log ("Flashlight Disappeared!");
-			
-			flashlight.renderer.enabled = false;
-			//flashlight.SetActive(false);			
-		}
-//		if(equippedItem!=flashlight)
-//		{
-//			renderer.enabled = false;
-//		}	
+			Debug.Log ("Flashlight Disappeared!");			
+			flashlight.renderer.enabled = false;		
+		}// End Update	
 	}
 }
+
