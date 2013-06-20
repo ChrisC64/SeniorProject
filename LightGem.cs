204e48ca45cbe665e2d071c472d0e7881ff728c1
Valtiaalle <ldiana.zhoukko@gmail.com>
2013-06-20 14:02:54 -0600

    Update LightGem.cs
    Add render conditions to the update function. It will check first if the item is even equipped before you can go in the update.

--- LightGem.cs
+++ LightGem.cs
@@ -15,34 +15,42 @@ public class LightGem : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if(Input.GetKeyDown(KeyCode.Mouse0)) {
-			if(b_IsOn) {
-				b_IsOn = false;
-			} else if(!b_IsOn) {
-				b_IsOn = true;
+	if(player.getSunGem() == true) {
+			sunStone.renderer.enabled = true;
+			Debug.Log ("Power of the sun!!!");	
+			if(Input.GetKeyDown(KeyCode.Mouse0)) {
+				if(b_IsOn) {
+					b_IsOn = false;
+				} else if(!b_IsOn) {
+					b_IsOn = true;
+				}
+				Debug.Log("Sun b_IsOn: " + b_IsOn);
 			}
-			Debug.Log("Sun b_IsOn: " + b_IsOn);
+			//check if the light is on
+			if(b_IsOn) {
+				Debug.DrawRay (transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward) * f_RayLength);
+				//Turn on light
+				
+				//If Object collides with other object that is lightObject, send message
+				if(Physics.Raycast(transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward), out hitInfo, f_RayLength)) {
+					//f_Distance = hitInfo.distance;	
+					if(hitInfo.collider.gameObject.tag == "LightObject") {
+						//send a message to switch states
+						lightObject = hitInfo.collider.gameObject;
+						lightObject.SendMessage("ActivateLight");
+						Debug.Log ("Light Gem object....");
+					}// End IF block
+				} // End Raycast
+			}else if(!b_IsOn){
+				//turn off light
+			}// End NOT block
+		} // End GETSUNGEM block
+		else if(player.getSunGem() == false)
+		{	
+			sunStone.renderer.enabled = false;
+			Debug.Log("Sun power down");	
 		}
-		//check if the light is on
-		if(b_IsOn) {
-			Debug.DrawRay (transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward) * f_RayLength);
-			//Turn on light
-			
-			//If Object collides with other object that is lightObject, send message
-			if(Physics.Raycast(transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward), out hitInfo, f_RayLength)) {
-				//f_Distance = hitInfo.distance;	
-				if(hitInfo.collider.gameObject.tag == "LightObject") {
-					//send a message to switch states
-					lightObject = hitInfo.collider.gameObject;
-					lightObject.SendMessage("ActivateLight");
-					Debug.Log ("Light Gem object....");
-				}// End IF block
-			} // End Raycast
-		}else if(!b_IsOn){
-			//turn off light
-		}// End IF block
-	}// End Update
+	}
 	
 	public bool GetIsOn() {return b_IsOn;}
 }
