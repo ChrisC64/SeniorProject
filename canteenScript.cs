c461e2ce474ad63c0c19fa685e2525a201002a94
Valtiaalle <ldiana.zhoukko@gmail.com>
2013-06-20 13:52:32 -0600

    Update canteenScript.cs

--- canteenScript.cs
+++ canteenScript.cs
@@ -7,28 +7,29 @@ using System.Collections;
 public class canteenScript : MonoBehaviour 
 {
 	public GameObject canteen;
+	public PlayerStates player;
 	
 //	public GameObject[] PlayerItems;
 	
 	// Use this for initialization
 	void Start () 
 	{
+		player = GameObject.FindWithTag("Player").GetComponent<PlayerStates>();
 		canteen.renderer.enabled = false;
 	} // End Start
 	
 	// Update is called once per frame
 	void Update () 
 	{	
-		if(Input.GetKeyDown("2") && canteen.renderer.enabled == false)
+		if(player.getCanteen() == true)
 		{
 			Debug.Log("Equipped canteen");					
 			canteen.renderer.enabled = true;	
 		}				
-		else if(Input.GetKeyDown("2") && canteen.renderer.enabled == true)
+		else if(player.getCanteen() == false)
 		{
 			Debug.Log("Canteen is gone?!");
 			canteen.renderer.enabled = false;
 		}	
 	} // End Update
 }
-
