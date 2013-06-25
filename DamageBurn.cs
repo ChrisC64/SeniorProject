9d8892fe3ff6189643584b309c8d81f22c529c4c
Christopher Carlos <yoshicube64@gmail.com>
2013-06-25 01:33:47 -0700

    Update DamageBurn.cs
    Added functionality to enabled and disabled traps. This should happen from a "Switch" class call, which is activated from "ActivateTrigger" class in the Player object.

--- DamageBurn.cs
+++ DamageBurn.cs
@@ -15,50 +15,63 @@ using UnityEngine;
 using System.Collections;
 
 public class DamageBurn : MonoBehaviour {
-  
+	
 	public float fixedDamage; //Damage over time
 	public float dTime; //Timer
-	
+	public float waitTime;
+	public bool b_IsActive;
 	private PlayerStates playerStates; //Player Class
+	
 	void Awake() {
 		playerStates = GameObject.FindWithTag("Player").GetComponent<PlayerStates>();
 	}
+	
 	// Use this for initialization
 	void Start () {
 		//set up parameters
-		fixedDamage = 1.0f;
-		dTime = 0.0f;
-		Debug.Log ("Variables Set! fixedDamage: " + fixedDamage + " dTime: " + dTime);
+		//fixedDamage = 1.0f;
+		//dTime = 0.0f;
+		waitTime = 0.001f;
 	} // End of Start()
 	
 	// Update is called once per frame
 	void Update () {
-		dTime = Time.deltaTime + dTime; // Timer
+		if(b_IsActive) { dTime = Time.deltaTime + dTime; } 
+		else if(!b_IsActive)
+		{ Debug.Log("No timer is needed!"); }
 
 	} // End of Update
 	
 	//Damage Over Time
 	void DamageOverTime(float time, float damage) {
 		//if passed time is greater than current time
-		if(time > 0.10f) {
-			Debug.Log ("Player has entered the pit! Player's Current HP: " + playerStates.getCurrHP());
+		if(playerStates.getIsAlive() == true && time > waitTime) {
 			//set damage
 			playerStates.takeDamage(damage);
 			//reset timer
 			dTime = 0.0f;
 		}
-		else {
-			Debug.Log ("Do nothing to this object!");	
-		}
+		else {	} // Do nothing
 	} // End of Damage Over Time
 	
 	void OnTriggerStay(Collider other) {
-		if(other.gameObject.tag == "Player") {
-			Debug.Log ("Object Player is staying in the pit!");
-			DamageOverTime(dTime, fixedDamage);	
+		if(b_IsActive)
+		{
+			if(other.gameObject.tag == "Player") {
+				Debug.Log ("Object Player is staying in the pit!");
+				DamageOverTime(dTime, fixedDamage);	
+			}
+			else {
+			}
+		} else if(!b_IsActive) {
+			Debug.Log ("Trap is off!");	
 		}
-		else {
-			Debug.Log ("Something is cooking! Not the player!");
-		}
+	} //End of OnTriggerStay 
+	
+	//Toggle Active state of object
+	void Toggle() { 
+		if(b_IsActive) { b_IsActive = false; } 
+		else if (!b_IsActive) { b_IsActive = true;}
 	}
+	
 } // End of DamageBurn
