6b57ba84a630d9a42641728431e18e4a75af58cf
Christopher Carlos <yoshicube64@gmail.com>
2013-08-04 01:11:04 -0700

    Update Switch.cs
    Added some new functions:
    
    -Activate() 
    When you connect a switch with another switch, it will enable that switch to be visible AND triggered. (Invisible switches cannot be hit)
    -Boolean bIsInvisible created; used for disappearing/reappearing invisible switches

--- Player/Switch.cs
+++ Player/Switch.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 public class Switch : MonoBehaviour {
-	private bool bIsSwitchedOn = true;
+	public bool bIsSwitchedOn;
 	public float targetTime = 2.0f;
 	public List<GameObject> connectedObjects = new List<GameObject>();
-	
+	public bool bIsInvisible;
+	//public GameObject switchObject;
 	//private float fCurrentTime = Time.deltaTime;
 	// Use this for initialization
 	void Start () {
-		
+		if(bIsSwitchedOn) {
+			this.renderer.material.color = Color.green;	
+		}
+		else if(!bIsSwitchedOn) {
+			this.renderer.material.color = Color.red;
+		}
+		if(bIsInvisible) {
+			//8 = invisible layer (doesn't appear on camera) and 9 = trigger
+			this.gameObject.layer = 8;
+			this.gameObject.collider.isTrigger = true;
+		}
 	}
 	
 	// Update is called once per frame
@@ -18,42 +29,45 @@ public class Switch : MonoBehaviour {
 	}
 	
 	void TriggerSwitch() {
-		//If switch is true AND time passed is greater than targetTime, switch off
+		//Toggle state of switch
 		if(bIsSwitchedOn)
 		{
 			Debug.Log ("Switch OFF!");
 			bIsSwitchedOn = false;
-			//Loop through contents
-			int count = 0;
-			foreach(GameObject objects in connectedObjects)
-			{
-				connectedObjects[count].renderer.enabled = false;
-				connectedObjects[count].collider.enabled = false;
-				count++;
-			}
+			this.renderer.material.color = Color.red;
 		}//Else if the switch if false AND the time passed is greater than targetTime, switch On
 		else if(!bIsSwitchedOn)
 		{
 			Debug.Log ("Switch ON");
 			bIsSwitchedOn = true;
-			//Loop through contents
-			int count = 0;
-			foreach(GameObject objects in connectedObjects)
-			{
-				connectedObjects[count].renderer.enabled = true;
-				connectedObjects[count].collider.enabled = true;
-				count++;
-			}
+			this.renderer.material.color = Color.green;
+		}
+		//Loop through contents and send message to connected objects
+		int count = 0;
+		foreach(GameObject objects in connectedObjects)
+		{
+			connectedObjects[count].SendMessage("Activate");
+			count++;
 		}
 	} // End TriggerSwitch
 	
-	void Activate() { 
-		Debug.Log("Watch Out Traps!");
-		int count = 0; 
-		foreach(GameObject objects in connectedObjects)
-		{
-			connectedObjects[count].SendMessage("Toggle");
-			count++;
+	public bool GetIsSwitchedOn () { return bIsSwitchedOn; }
+	
+	public void SetIsSwtichedOn (bool setSwitch ) { bIsSwitchedOn = setSwitch; }
+	
+	public void Activate() {
+		if(bIsInvisible) {
+			//disappear object and make a trigger to avoide collsion
+			//8 = invisible layer (doesn't appear on camera) and 9 = trigger
+			bIsInvisible = false;
+			this.gameObject.layer = 9;
+			this.gameObject.collider.isTrigger = false;
+		}
+		else if (!bIsInvisible) {
+			//reappear object and set collision back on
+			bIsInvisible = true;
+			this.gameObject.layer = 8;
+			this.gameObject.collider.isTrigger = true;
 		}
 	}
 }
