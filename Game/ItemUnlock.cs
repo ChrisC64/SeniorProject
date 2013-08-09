2f0b9845aefccf6f7d63ce41a732671d75e34268
Christopher Carlos <yoshicube64@gmail.com>
2013-08-09 01:41:31 -0700

    Create ItemUnlock.cs
    This is used with the updated PlayerStates class. 
    
    -Players will "find" the GemStones and unlock them (able to equip)
    
    This one is for the SuneGem, the other for the MoonGem is ItemUnlockMoon

---
+++ Game/ItemUnlock.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class ItemUnlock : MonoBehaviour {
+  public GameObject item;
+	private PlayerStates playerState;
+	void Awake() {
+		playerState = GameObject.FindWithTag("Player").GetComponent<PlayerStates>();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void Activate() {
+		//when player activates this object, we set the value to TRUE for the item and destroy the object
+		playerState.setUnlockSun(1);
+		playerState.equipItem (2);
+		item.SetActive(false);
+	}
+}
