c184386d8bbc207af8927bdfb94cd49d59b66a68
Jimmy Tjandra <jimmy.tjandra.jobs.@gmail.com>
2013-08-12 15:45:49 -0700

    Footstep Update
    - Made the code simpler and far less heavy to process.
    - It's only for a single set of footsteps only.

--- Player/Footsteps.js
+++ Player/Footsteps.js
@@ -9,93 +9,82 @@
 //////////////
 //Make another audioclip array for different materials(i.e rock, wood, etc.)
 var defaultClip : AudioClip[];
-var dirt : AudioClip[];
+//var dirt : AudioClip[];
+var controller : CharacterController; 
 
 private var movementState : int;
 private var step : boolean = true;
 var audioWalkTimer : float = 0.45; 	//Delay for walking
-var audioRunTimer : float = 0.25;	//Delay for running
+var audioRunTimer : float = 0.25;	//Delay for running 
+
+function Start()
+{
+	 controller = GetComponent(CharacterController);
+}
 
 
 //Movement sounds/steps
-function OnControllerColliderHit (hit : ControllerColliderHit)
-{
-	Debug.Log("Check");
-	var controller : CharacterController = /*GameObject.FindGameObjectWithTag("Player").*/GetComponent(CharacterController);
+function Update()
+{	
+	if(controller.isGrounded) 
+	//if(controller.velocity.y<1&&controller.velocity.y>-0.5)
+	{ 
+		if(controller.velocity.magnitude>1)
+		{
+			//If the player is on the ground AND player's speed is between 7 and 5(walking) AND step is true....
+			if(step)
+			{
+				Default();
+			}
+		}		
+	}
 	
 	//determine if player is walking or running
-	Debug.Log("Magnituge: "+controller.velocity.magnitude);
-	if(controller.velocity.magnitude==0) 
-	{
-		movementState = 0; 		 //is idle
-	}
-	else if(controller.velocity.magnitude>5 && controller.velocity.magnitude<7)
-	{
-		movementState = 1;		//is walking
-	}
-	else if(controller.velocity.magnitude>7)
-	{
-		movementState = 2;		//is running
-	}
-		
-	
-	//If the player is on the ground AND player's speed is between 7 and 5(walking) AND step is true....
-	if(controller.isGrounded && step)
-	{
-		switch(hit.gameObject.tag)
-		{
-		case "Dirt":
-			Dirt(movementState);
-			break;			
-		default:
-			Default(movementState);
-			break;
-		}
-	}
+//	if(controller.velocity.magnitude==0) 
+//	{
+//		movementState = 0; 		 //is idle
+//	}
+//	else if(controller.velocity.magnitude>5 && controller.velocity.magnitude<7)
+//	{
+//		movementState = 1;		//is walking
+//	}
+//	else if(controller.velocity.magnitude>7)
+//	{
+//		movementState = 2;		//is running
+//	}
 }
 
 //The default sound just in case ground is untagged
-function Default(state: int)
-{ 
-	if(state!=0) 
-	{
-		step = false;
-		//adds some randomization if more than one clip is inserted to the array
-		audio.clip = defaultClip[Random.Range(0, defaultClip.length)];
-		audio.volume = 0.1;
-		audio.Play();
-		switch(state)
-		{
-		case 1:
-			yield WaitForSeconds(audioWalkTimer);
-			break;
-		case 2:
-			yield WaitForSeconds(audioRunTimer);
-			break;
-		}
-	}		
+function Default()
+{ 	
+	step = false;
+	//adds some randomization if more than one clip is inserted to the array
+	audio.clip = defaultClip[Random.Range(0, defaultClip.length)];
+	audio.volume = 0.1;
+	audio.Play(); 
+	yield WaitForSeconds(audioWalkTimer);	
 	step = true;
 }
 
 //If player walks on dirt
-function Dirt(state: int)
-{
-	if(state!=0) 
-	{
-		step = false;
-		//adds some randomization if more than one clip is inserted to the array
-		audio.clip = dirt[Random.Range(0, dirt.length)];
-		audio.volume = 0.1;
-		audio.Play();
-		switch(state)
-		{
-		case 1:
-			yield WaitForSeconds(audioWalkTimer);
-			break;
-		case 2:
-			yield WaitForSeconds(audioRunTimer);
-			break;
-		}
-	}		
-	step = true;
-}
+//function Dirt(state: int)
+//{
+//	if(state!=0) 
+//	{
+//		step = false;
+//		//adds some randomization if more than one clip is inserted to the array
+//		audio.clip = dirt[Random.Range(0, dirt.length)];
+//		audio.volume = 0.1;
+//		audio.Play();
+//		switch(state)
+//		{
+//		case 1:
+//			yield WaitForSeconds(audioWalkTimer);
+//			break;
+//		case 2:
+//			yield WaitForSeconds(audioRunTimer);
+//			break;
+//		}
+//	}		
+//	step = true;
+//}
