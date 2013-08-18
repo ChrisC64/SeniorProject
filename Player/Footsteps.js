cec4ba39c248fcea2598afb5bfc5c770f5c3fc4d
Christopher Carlos <yoshicube64@gmail.com>
2013-08-18 12:52:00 -0700

    Update Footsteps.js
    -Adjusted volume to 0.25 from 0.1 due to being a bit too low for our custom sounds.

--- Player/Footsteps.js
+++ Player/Footsteps.js
@@ -38,20 +38,6 @@ function Update()
 			}
 		}		
 	}
-	
-	//determine if player is walking or running
-//	if(controller.velocity.magnitude==0) 
-//	{
-//		movementState = 0; 		 //is idle
-//	}
-//	else if(controller.velocity.magnitude>5 && controller.velocity.magnitude<7)
-//	{
-//		movementState = 1;		//is walking
-//	}
-//	else if(controller.velocity.magnitude>7)
-//	{
-//		movementState = 2;		//is running
-//	}
 }
 
 //The default sound just in case ground is untagged
@@ -60,31 +46,8 @@ function Default()
 	step = false;
 	//adds some randomization if more than one clip is inserted to the array
 	audio.clip = defaultClip[Random.Range(0, defaultClip.length)];
-	audio.volume = 0.1;
+	audio.volume = 0.25;
 	audio.Play(); 
 	yield WaitForSeconds(audioWalkTimer);	
 	step = true;
 }
-
-//If player walks on dirt
-//function Dirt(state: int)
-//{
-//	if(state!=0) 
-//	{
-//		step = false;
-//		//adds some randomization if more than one clip is inserted to the array
-//		audio.clip = dirt[Random.Range(0, dirt.length)];
-//		audio.volume = 0.1;
-//		audio.Play();
-//		switch(state)
-//		{
-//		case 1:
-//			yield WaitForSeconds(audioWalkTimer);
-//			break;
-//		case 2:
-//			yield WaitForSeconds(audioRunTimer);
-//			break;
-//		}
-//	}		
-//	step = true;
-//}
