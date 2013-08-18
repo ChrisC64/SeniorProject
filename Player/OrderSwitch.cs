ead898f3f1f43390859a405b148f798f647212b7
Christopher Carlos <yoshicube64@gmail.com>
2013-08-18 12:56:20 -0700

    Create OrderSwitch.cs
    -Used with OrderSwitchObject.cs script
    -This belongs to a "switch" item, where it sends a message to see if the switch is the next one on the list. If not, the item's state will revert back to normal.

---
+++ Player/OrderSwitch.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class OrderSwitch : MonoBehaviour {
+	private bool bIsOn;
+	private bool bNoMoreFlips;
+	public List<GameObject> orderSwitch;
+	// Use this for initialization
+	void Start () 
+	{
+		bIsOn = false;
+		bNoMoreFlips = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(bNoMoreFlips)
+		{
+			if(bIsOn)
+			{
+				this.renderer.material.color = Color.white;	
+			}
+			else if(!bIsOn)
+			{
+				this.renderer.material.color = Color.white;
+			}
+		}
+	}
+	
+	//when activated switch state and send message to object
+	void Activate() 
+	{
+		if(!bNoMoreFlips)
+		{
+			bIsOn = true;
+			for(int i = 0; i < orderSwitch.Count; i++)
+			{
+				orderSwitch[i].SendMessage("Check", this.gameObject);
+			}
+		}
+	}
+	
+	void Reset() 
+	{
+		bIsOn = false;
+		
+	}
+	
+	void StopFlips()
+	{
+		bNoMoreFlips = true;	
+	}
+	
+}
