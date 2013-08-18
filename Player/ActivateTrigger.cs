c709d5749256e59cc798cafa343baf7c83f400f6
Christopher Carlos <yoshicube64@gmail.com>
2013-08-18 12:50:06 -0700

    Update ActivateTrigger.cs
    -When player hits an item to interact with, the cursor turns green

--- Player/ActivateTrigger.cs
+++ Player/ActivateTrigger.cs
@@ -14,6 +14,9 @@ public class ActivateTrigger : MonoBehaviour {
 	public RaycastHit hitInfo = new RaycastHit();
 	public float f_rayLength = 4.0f;
 	private bool b_ObjectCollision;
+	public Texture2D cursor;
+	public Texture2D greenCursor;
+	public LayerMask hitLayer;
 	//public float fTargetTime = 2.0f;
 	
 	private float fCurrentTime = 0.0f;
@@ -22,61 +25,126 @@ public class ActivateTrigger : MonoBehaviour {
 	
 	//private float dTime = Time.deltaTime;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		//update time
 		fCurrentTime = Time.deltaTime + fCurrentTime;
 		//Debug 
 		
 		Debug.DrawRay(transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward) * f_rayLength);
-		if(Physics.Raycast(transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward), out hitInfo, f_rayLength)) {
-			
-			switch(hitInfo.collider.gameObject.tag.ToString()) {
+		if(Physics.Raycast(transform.position, Camera.mainCamera.transform.TransformDirection(Vector3.forward), out hitInfo, f_rayLength, hitLayer)) 
+		{
+			switch(hitInfo.collider.gameObject.tag.ToString()) 
+			{
 			case("Switch"):
 				b_ObjectCollision = true;
 				Debug.Log("This is a switch!");
-				if(Input.GetButtonDown ("Action"))	{
+				if(Input.GetButtonDown ("Action"))	
+				{
 					// Send message to switchTrigger and initiate "switch"
 					switchCollision = hitInfo.collider.gameObject;
 					switchCollision.SendMessage("TriggerSwitch");
 				}
 				break;
-			case("Trap"):
+			case("Item"):
 				b_ObjectCollision = true;
-				Debug.Log("Trap! Watch out!");
-				if(Input.GetButtonDown("Action")) {
+				Debug.Log("Item in sight!");
+				if(Input.GetButtonDown("Action"))
+				{
 					switchCollision = hitInfo.collider.gameObject;
 					switchCollision.SendMessage("Activate");
 				}
 				break;
+			case("Order"):
+				b_ObjectCollision = true;
+				Debug.Log ("Order switch!");
+				if(Input.GetButtonDown ("Action")) 
+				{
+					switchCollision = hitInfo.collider.gameObject;
+					switchCollision.SendMessage("Activate");
+				}
+				break;
+			case("Door"):
+				b_ObjectCollision = true;
+				Debug.Log ("Door in sight!");
+				if(Input.GetButtonDown ("Action")) 
+				{
+					switchCollision = hitInfo.collider.gameObject;
+					switchCollision.SendMessage("OpenDoor");
+				}
+				break;
+			case("MoveSwitch"):
+				b_ObjectCollision = true;
+				Debug.Log ("Door in sight!");
+				if(Input.GetButtonDown ("Action"))
+				{
+					switchCollision = hitInfo.collider.gameObject;
+					switchCollision.SendMessage("TriggerSwitch");
+				}
+				break;
 			default:
-				Debug.Log ("Nothing special here...");
+				b_ObjectCollision = false;
 				break;
 			}
-		} else {
+		} 
+		else
+		{
 			b_ObjectCollision = false;
 		}
 		//save current time and time passed since
-		if(fCurrentTime > 2.0f)	{
+		if(fCurrentTime > 2.0f)	
+		{
 			fCurrentTime = 0.0f;
-			}
+		}
 	}
 	
 	
 	
-	void OnGUI() {
-		if(b_ObjectCollision) {
+	void OnGUI() 
+	{
+		if(b_ObjectCollision) 
+		{
 			GUI.BeginGroup(new Rect(Screen.width * 0.5f, Screen.height * 0.65f,  Screen.width * 0.70f, Screen.height * 0.80f));
 			GUI.Box(new Rect(0, 0, 175, 30), "");
+			switch(hitInfo.collider.gameObject.tag.ToString()) 
+			{
+			case("Switch"):
 			GUI.Label(new Rect(5, 5, Screen.width * 0.90f, Screen.height * 0.90f), "Press E to Activate " 
-				+ hitInfo.collider.gameObject.tag.ToString());	
+				+ hitInfo.collider.gameObject.tag.ToString());
+				break;
+			case("Item"):
+				GUI.Label (new Rect(5, 5, Screen.width * 0.90f, Screen.height * 0.90f), "Press E to Collect "
+					+ hitInfo.collider.gameObject.tag.ToString());
+				break;
+			case("Order"):
+				GUI.Label (new Rect(5, 5, Screen.width * 0.90f, Screen.height * 0.90f), "Press E to Flip " + switchCollision.gameObject.name);
+				break;
+			case("Door"):
+				GUI.Label (new Rect(5, 5, Screen.width * 0.90f, Screen.height * 0.90f), "Press E to Open Door");
+				break;
+			case("MoveSwitch"):
+				GUI.Label (new Rect(5, 5, Screen.width * 0.90f, Screen.height * 0.90f), "Press E to flip the switch");
+				break;
+			}
 			GUI.EndGroup();
-		} else if(!b_ObjectCollision) {
-			// Nothin
+		} 
+		else if(!b_ObjectCollision) 
+		{
+			// Nothing
+		}
+		if(b_ObjectCollision)
+		{
+			GUI.DrawTexture(new Rect( Screen.width / 2, Screen.height / 2, 32, 32), greenCursor);
+		}
+		else
+		{
+			GUI.DrawTexture(new Rect( Screen.width / 2, Screen.height / 2, 32, 32), cursor);
 		}
 	}
 }
