93a1625d22fa993c3e2a0edae976f6e991a36ed8
Christopher Carlos <yoshicube64@gmail.com>
2013-08-18 12:45:59 -0700

    Update BossScript.js
    -Fixed bugs and problems
    -Added Animation

--- Boss Scripts/BossScript.js
+++ Boss Scripts/BossScript.js
@@ -1,62 +1,102 @@
-
+#pragma strict
 
 //Variables
 var whiteMissilePrefab : GameObject;
 var darkMissilePrefab : GameObject;
-var spawnTime : float = 1.0f;
+var endDoor : GameObject;
+var moveDoor;
+//floats
+private var startTimer : float;
+var spawnTime : float = 3.0f;
 var spawnRate : float = 5.0f; 
-var spiritMax : int = 5;
+//ints
+var spiritMax : int = 1;
 var spiritCount : int = 0; 
-var canSpawn : boolean = true;
 var bossHealth : int = 5; 
-
 var STYLE : int = 0;
+//booleans
+var canSpawn : boolean = true;
+private var bPlayerArrives : boolean = false;
+private var bIsDead : boolean = false;
+
 
 function Start () 
 {
 	whiteMissilePrefab = Resources.Load("White Spirit") as GameObject;
 	darkMissilePrefab = Resources.Load("Dark Spirit") as GameObject;
+	startTimer = 3.0f;
+	moveDoor = endDoor.GetComponent("MoveObject");
 }
 
 function Update () 
 {	 
-	if(bossHealth<=0)
+	if(bPlayerArrives)
 	{
-		Destroy(gameObject.Find("BossMesh").gameObject);
-	} 
-	if(spiritCount>=spiritMax)
-		canSpawn = false;
-	else if(bossHealth>0) 
-	{
-		canSpawn = true;
-		spawnTime -= Time.deltaTime;  
-	}
-	
-	
-	//Will only spawn more once the previous wave is all gone
-	if(canSpawn)
-	{
-		if(spawnTime <= 0)
-		{ 
-			spawnTime = spawnRate;
-				
-			switch(STYLE)
+		if(startTimer <= 0)
+		{
+			if(bossHealth<=0 && !bIsDead)
 			{
-			case 0: 
-				SpawnWave(spiritMax);
-				
-				break; 
-			case 1:	 
-				//Broken
-				SpawnSpam(spiritMax); 
-				break;
-	
-			
+				endDoor.tag = "Door";
+				this.gameObject.animation.Play("Death_Mateas");
+				bIsDead = true;
+			} 
+			/*else if(bossHealth>0) //never enter here after spiritCount >= spiritMax; logical error
+			{
+				canSpawn = true;
+				spawnTime -= Time.deltaTime;  
+			}*/
+			if(spiritCount>=spiritMax)
+			{
+				canSpawn = false;
 			}
+			else if(spiritCount <= spiritMax)
+			{
+				canSpawn = true;
+				spawnTime -= Time.deltaTime;
+			} 
 			
-			//Play audio clip 
-			audio.Play();
-		} 
+			//this.animation.Play("Idle_Mateas");
+			//Will only spawn more once the previous wave is all gone
+			if(canSpawn && spawnTime <= 0 && !bIsDead) //check if spawn time allows you to spawn more
+			{
+				if(!this.gameObject.animation.IsPlaying("Summon"))
+				{
+					this.gameObject.animation.Play("Summon");
+				}
+				spawnTime = spawnRate;
+						
+				switch(STYLE)
+				{
+				case 0: 
+					Debug.Log("SpiritCount: " + spiritCount + " Style 0");
+					spiritCount++;
+					SpawnWave(1);
+					break; 
+				case 1:	 
+					//Broken
+					Debug.Log("SpiritCount: " + spiritCount + " Style 1");
+					spiritCount++;
+					SpawnSpam(1); 
+					break;
+				}
+					
+				//Play audio clip 
+				/*if(!audio.IsPlaying())
+				{
+					audio.Play();
+				}
+				else if(audio.IsPlaying())
+				{ audio.Pause();}*/
+			} 
+			
+			if(!animation.IsPlaying("Death_Mateas") && bIsDead)
+			{
+				Destroy(gameObject.FindWithTag("Boss").gameObject);
+				
+			}
+		}
+		startTimer -= Time.deltaTime;
+		Debug.Log("start time: " + startTimer);
 	}
 }
 
@@ -71,8 +111,8 @@ function SpawnWave(num:int)
 	var rand : int;
 	var newMissile : GameObject;
  
- 	for(var i:int=0; i<num; i++)
- 	{
+ 	//for(var i:int=0; i<num; i++)
+ 	//{
 		rand = Random.Range(0,2);
 		if(rand==0) 
 		{ 
@@ -86,8 +126,8 @@ function SpawnWave(num:int)
 			newMissile.SendMessage("RandomRise", SendMessageOptions.DontRequireReceiver); 
 			//spiritCount++; 
 		}
-		spiritCount++;  
-	} 
+		//spiritCount++;  
+	//} 
 } 
 
 function SpawnSpam(num:int)
@@ -95,8 +135,8 @@ function SpawnSpam(num:int)
 	var rand : int;
 	var newMissile : GameObject; 
 	
-	if(num>0)
-	{
+	//if(num>0)
+	//{
 		rand = Random.Range(0,2);
 		if(rand==0) 
 		{ 
@@ -109,17 +149,29 @@ function SpawnSpam(num:int)
 			newMissile.SendMessage("RandomRise", SendMessageOptions.DontRequireReceiver);
 		}
 		
-		spiritCount++; 
-		num--;
-	} 
+		//spiritCount++; 
+		//num--;
+	//} 
 } 
  
 //For when the boss gets damaged. Increment difficulty. 
-function Hurt()
+function Activate()
 {
-	spiritMax += 5; 
+	//spiritMax++; 
+	spawnRate = spawnRate / 2 ;
 	bossHealth--;
+	if(bossHealth > 0 )
+	{
+		this.gameObject.animation.Play("Damage_Mateas");
+	}
+	else if (bossHealth <= 0)
+	{
+		this.gameObject.animation.Play("Knockback");
+	}
 }
 
-
+function StartTimer()
+{
+	bPlayerArrives = true;
+}
 
